bf371b684a0740b86c288403acb5011cff9d8ce5
Victor <comrazvictor@gmail.com>
2022-11-19 23:24:42 +0200

    Update mathmod_test.go

--- mathmod/mathmod_test.go
+++ mathmod/mathmod_test.go
@@ -8,7 +8,7 @@ import (
 // add(5, 8) should return 13
 func TestAdd(t *testing.T) {
     z := Add(5, 8)
-    if z != 15 {
+    if z != 13 {
         t.Fatalf("add(5, 8) function did not return 13.")
     }
 }
